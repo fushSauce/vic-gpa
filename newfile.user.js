19e024e9e518d5084141280c0f684a51a18eac9a
Micky Snadden <snaddemick@ecs.vuw.ac.nz>
2022-12-04 18:26:58 +1300

    second commit

--- newfile.user.js
+++ newfile.user.js
@@ -2,208 +2,211 @@
 // @name        New script
 // @namespace   Violentmonkey Scripts
 // @match *://student-records.vuw.ac.nz/*.P_FacStuInfo
-// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
-// @grant       none
 // @version     1.0
 // @author      qut3
+// @grant       unsafeWindow
 // @description 28/11/2022, 16:08:13
 // ==/UserScript==
 
 
-const gradeTableList = document.querySelectorAll('[summary="This table displays the student course history information."]') // First element
-const termTableList = document.querySelectorAll('[summary="This table displays the student degree history information."]');
-
-const INCLUDE_ZEROS = Boolean(false);
-const GRADE_POINTS_COL_INDEX = 7;
-const TERM_INDEX = 1;
+const IGNORE_ZEROS = true
 
 /**
- * gets gpa from a table by iterating grade point column and averaging
+ * map of grade points corresponding letter values.
+ * @type {Map<number, string>}
+ */
+const gradePointMap = new Map([
+    [0,"D"],
+    [1, "C-"],
+    [2, "C"],
+    [3, "C+"],
+    [4, "B-"],
+    [5, "B"],
+    [6, "B+"],
+    [7, "A-"],
+    [8, "A"],
+    [9, "A+"]
+])
+
+/**
+ * All term numbers featured on the page
+ * @type {number[]}
+ */
+const termTablesArray = [...document.querySelectorAll('table[summary="This table displays the student degree history information."] tbody tr td')];
+let termArray = termTablesArray.map(e => parseInt(e.textContent)).filter(e => {
+    return !isNaN(e);
+});
+console.log("slkdfj",termTablesArray)
+
+/**
+ * Map of terms to corresponding gpa of given term.
+ * @type {Map<any, any>}
+ */
+const termGPAMap = initTermGPAMap();
+function initTermGPAMap() {
+    const termGPAMap = new Map();
+    for (let termNumber of termArray) {
+        termGPAMap.set(termNumber,getGPAFromTerm(termNumber))
+    }
+    return termGPAMap;
+}
+
+/**
+ * Get grade table that is below term table with given term number.
+ * @param termNumber
+ */
+function getTermGradeTable(termNumber) {
+    const element = document.querySelectorAll('table[summary="This table displays the student degree history information."]');
+    const slice = [...element].slice(1, [...element].length); // don't want first table as it's essentially acting as a header
+    for (let table of slice) {
+        const termFromTable = Number(parseInt(table.querySelector('tbody').querySelector('td').querySelector('p').querySelector('b').innerText));
+        if (termFromTable === termNumber) {
+            return table.nextElementSibling;
+        }
+    }
+}
+
+/**
+ * Gets table rows and create new objects from their cells text contents.
  * @param table
- * @returns {number}
- */ function getGPA(table) {
-    const tRows = table.tBodies[0].rows;
-    let sum = 0;
-    let count = 0;
-    for (let row =1; row <tRows.length; row++) {
-        const cells = tRows.item(row).cells;
-        const gradePointText = cells.item(GRADE_POINTS_COL_INDEX).textContent;
-        const gradePointInt = Number(parseFloat(gradePointText));
-        if (isNaN(gradePointInt) ) { continue }
-        if (gradePointInt === 0) {
+ */
+function getTableAsObjects(table) {
+    const rows = table.rows;
+    const header = rows.item(0);
+    const list = [];
+    for (let row of rows) {
+        if (row === header) {
             continue;
         }
-        sum+=gradePointInt;
-        count++;
+        const object = {};
+        const cells = row.cells;
+        for (let cell of cells) {
+            const headerValue = header.cells.item(cell.cellIndex).textContent;
+            object[headerValue] = cell.textContent;
+        }
+        object["term"] = table.previousElementSibling.querySelector('tbody td p b').textContent;
+        list.push(object);
     }
-    const res = Number(parseFloat(sum / count).toFixed(2));
-    return isNaN(res) ? 0 : res;
+    return list;
+}
+
+function getAllTablesAsObjects() {
+    let listOfAllTablesCombined = [];
+    for (let term of termArray) {
+        const termGradeTable = getTermGradeTable(term);
+        const tableAsObjects = getTableAsObjects(termGradeTable);
+        for (let entry of tableAsObjects) {
+            listOfAllTablesCombined.push(entry);
+        }
+    }
+    return listOfAllTablesCombined;
+}
+
+window.getAllTablesAsJson = ()=> {
+    let bigObj = {};
+    const allTablesAsObjects = getAllTablesAsObjects();
+    for (let obj of allTablesAsObjects) {
+        console.log(allTablesAsObjects.indexOf(obj))
+        bigObj[allTablesAsObjects.indexOf(obj)] = obj;
+    }
+    return bigObj;
+}
+
+getAllTablesAsJson();
+/**
+ * Returns GPA of given term.
+ * @param termNumber
+ * @returns {number|string}
+ */
+function getGPAFromTerm(termNumber) {
+    const termGradeTable = getTermGradeTable(termNumber);
+    const tableAsObjs = getTableAsObjects(termGradeTable);
+    let gradesArray = tableAsObjs.map(e => {
+        return Number(parseInt(e["GradePts"]));
+    }).filter(e => {
+        if (IGNORE_ZEROS && e === 0) {
+            return false;
+        }
+        return !isNaN(e);
+    });
+    const gradesArraySum = gradesArray.reduce((a, b) => a + b, 0);
+    const gpa = (gradesArraySum/gradesArray.length).toFixed(2);
+    return isNaN(gpa) ? 0 : gpa;
 }
 
 /**
- * Goes through all gradeTables and gets average
- * @param tableList
- * @returns {number}
+ * Averages GPAs of each term to get overall GPA.
+ * @returns {string}
  */
-function getFullGPA(tableList) {
-    let sum = 0;
-    let count = 0;
-    for (let tableInd=0; tableInd < tableList.length;tableInd++) {
-        var table = tableList.item(tableInd);
-        var gpa = getGPA(table);
-        gpa = Number(gpa);
-        if (!INCLUDE_ZEROS && gpa === 0) {
+function getOverallGPA() {
+    let gpaArray = Array.from(termGPAMap.values()).filter(e=>{
+        return !(IGNORE_ZEROS && e === 0);
+    }).map(e=>Number(e));
+    const gpaSum = gpaArray.reduce((a, b) => a+b, 0);
+    let gpa = (gpaSum/gpaArray.length).toFixed(2);
+    return gpa;
+}
+
+/**
+ * Sets up GPA section below Qualification Status section.
+ */
+function setUpGPASection() {
+    let gpaTableHTML =
+        "<br>" +
+        "<hr>" +
+        "<table class='datadisplaytable' id='gpa_table' summary=\"This table displays the student gpa status information.\">" +
+        "<caption class=\"captiontext\"><a name=\"gpa_stat\">GPA Status&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a id='overall-gpa'>Overall: VAL,CLG: VAL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href=\"#top\" alt=\"TOP\">-Top-</a></caption>\n" +
+        "<tbody><tr>\n" +
+        "<th colspan=\"1\" class=\"ddheader\" scope=\"col\">Term</th>\n" +
+        "<th colspan=\"1\" class=\"ddheader\" scope=\"col\">GPA</th>\n" +
+        "<th colspan=\"1\" class=\"ddheader\" scope=\"col\">Closest Letter Grade</th>\n" +
+        "</tr>" +
+        "<tr>\n" +
+        "\n" +
+        "\n" +
+        "</tr>\n" +
+        "</tbody></table>";
+
+    const qualificationStatusTable = document.querySelector('table[summary="This table displays the student qualification status information."]');
+    qualificationStatusTable.insertAdjacentHTML("afterend",gpaTableHTML);
+
+    for (let term of termArray) {
+        const gpaFromTerm = getGPAFromTerm(term);
+        const gpaTable = document.querySelector('#gpa_table');
+        const htmlTableRowElement = gpaTable.insertRow(gpaTable.rows.length);
+        const termCell = htmlTableRowElement.insertCell(0);
+        termCell.className = "dddefault"
+        termCell.textContent = term;
+        termCell.style.width='100'
+        const gpaPtsCell = htmlTableRowElement.insertCell(1);
+        gpaPtsCell.className = "dddefault"
+        gpaPtsCell.textContent = gpaFromTerm;
+        gpaPtsCell.style.width='100'
+        const gpaLetterCell = htmlTableRowElement.insertCell(2);
+        gpaLetterCell.style.width='150'
+        gpaLetterCell.className = "dddefault"
+        if (gpaFromTerm === 0) {
+            gpaLetterCell.textContent = "NA";
             continue;
         }
-        sum+=gpa;
-        count++;
+        gpaLetterCell.textContent = gradePointMap.get(Math.round(gpaFromTerm));
     }
-    return Number(parseFloat(sum/count).toFixed(2));
+
+    document.querySelector('#overall-gpa').innerText = `Overall: ${getOverallGPA()}, CLG: ${gradePointMap.get(Math.round(Number(getOverallGPA())))}`;
+    document.querySelector('#overall-gpa').appendChild( document.createTextNode( '\u00A0\u00A0\u00A0\u00A0\u00A0' ) );
+
+    const thesisInfoElement = document.querySelector('a[href="#thesis_info"]');
+    thesisInfoElement.insertAdjacentHTML('afterend',"<a href=\"#gpa_stat\">GPA Status</a>")
+    document.querySelector('a[href="#gpa_stat"]').insertAdjacentHTML('beforebegin'," &nbsp;&nbsp; ")
 }
 
-/**
- * Term 202001
- *  gpa: 7
- * Term 202101
- *  gpa: 6.5
- * Term 202201
- *  gpa: 5.83
- * Full gpa: (5.83+6.5+7)/3 = 6.44
- */
+setUpGPASection();
 
-/**
- * gets table above course table showing the term and degree
- * @param term
- * @returns {Element}
- */
-function getTermTable(term) {
-    console.log(termTableList)
-    for (let termIndex = 0; termIndex < termTableList.length; termIndex++) {
-        const termTable = termTableList.item(termIndex);
-        const termTableBody = termTable.tBodies[0];
-        const termTableRow = termTableBody.rows[0];
-        const rowCells = termTableRow.cells;
-        if (rowCells.length < 4) {continue}
-        const termTextContent = rowCells.item(TERM_INDEX).childNodes.item(0).childNodes.item(0).textContent;
-        if (term === termTextContent) {
-            return termTable;
-        }
-    }
-}
-
-/**
- * gets GPA of given term string
- * @param term
- * @returns {number}
- */
-function getTermGPA(term) {
-    const termTable = getTermTable(term);
-    const courseTable = termTable.nextElementSibling;
-    return getGPA(courseTable);
-}
-
-function generateTermGPAMap() {
-    const map = new Map();
-    for (let termIndex = 0; termIndex < termTableList.length; termIndex++) {
-        const termTable = termTableList.item(termIndex);
-        const termTableBody = termTable.tBodies[0];
-        const termTableRow = termTableBody.rows[0];
-        const rowCells = termTableRow.cells;
-        if (rowCells.length < 4) {continue}
-        const termTextContent = rowCells.item(TERM_INDEX).childNodes.item(0).childNodes.item(0).textContent;
-        map.set(termTextContent,getTermGPA(termTextContent))
-    }
-    return map;
-}
-function testFunc() {
-    var termGPAMap = generateTermGPAMap();
-    termGPAMap.set("overall",getFullGPA(gradeTableList))
-    var obj = Object.fromEntries(termGPAMap);
-    var jsonString = JSON.stringify(obj,null,'\t');
-
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
-
-    const captionElement = document.createElement('caption');
-    captionElement.setAttribute("class","captiontext")
-    table.appendChild(captionElement);
-
-    const a1 = document.createElement('a');
-    a1.setAttribute('name','gpa_stat')
-    a1.innerHTML = "GPA Status&nbsp;&nbsp;&nbsp;&nbsp;"
-    const a2 = document.createElement('a');
-    a2.setAttribute('href','#top');
-    a2.setAttribute('alt','TOP');
-    a2.innerHTML = "-Top-"
-    captionElement.appendChild(a1);
-    captionElement.appendChild(a2)
-
-    table.appendChild(thead);
-    table.appendChild(tbody);
-
-    table.setAttribute('class','datadisplaytable')
-    table.setAttribute("summary","This table displays the student qualification status information.")
-    table.setAttribute('width','300')
+const allTablesAsJson = getAllTablesAsJson();
 
 
-    const qualElement = document.querySelector('[summary="This table displays the student qualification status information."]');
-    const br = qualElement.insertAdjacentElement('afterend', document.createElement('br'));
-    const hr = br.insertAdjacentElement('afterend', document.createElement('hr'));
-    table = hr.insertAdjacentElement('afterend', table);
-
-    const row0 = tbody.insertRow(0);
-    const th1 = document.createElement('th');
-    th1.setAttribute('colspan',1);
-    th1.setAttribute('class','ddheader');
-    th1.setAttribute('scope','col')
-    th1.innerHTML = "Term"
-
-    const th2 = document.createElement('th');
-    th2.setAttribute('colspan',1);
-    th2.setAttribute('class','ddheader');
-    th2.setAttribute('scope','col')
-    th2.innerHTML = "GPA"
-
-    row0.appendChild(th1);
-    row0.appendChild(th2);
-
-
-    let count = 1;
-    for (let [key, value] of termGPAMap.entries()) {
-        console.log("key: " + key)
-        const row = tbody.insertRow(count);
-
-
-        const td1 = document.createElement('td');
-        td1.setAttribute('colspan',1);
-        td1.setAttribute('class','dddefault');
-        td1.setAttribute('scope','col')
-        td1.innerHTML = key
-
-        row.appendChild(td1);
-
-        const td2 = document.createElement('td');
-        td2.setAttribute('colspan',1);
-        td2.setAttribute('class','dddefault');
-        td2.setAttribute('scope','col')
-        td2.innerHTML = value
-
-        row.appendChild(td2)
-
-        count++;
-    }
-}
-
-function addGPAButton() {
-    // const thesisLink = document.querySelector('[href="#thesis_info"]');
-    // thesisLink.insertAdjacentHTML("afterend",'\n' + '&nbsp;&nbsp;\n <a href="javascript:" fart="gpa-link">GPA</a>');
-    // const gpaLink = document.querySelector('[fart="gpa-link"]');
-    // gpaLink.addEventListener("click",()=>{
-    //     testFunc();
-    // })
-    testFunc();
-
-}
-addGPAButton();
-
+var oldFunction = unsafeWindow.SomeFunctionInPage;
+unsafeWindow.SomeFunctionInPage = function(text) {
+    alert('Hijacked! Argument was ' + text + '.');
+    return oldFunction(text);
+};
